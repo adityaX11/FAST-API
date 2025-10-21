f7cc8528d533168513f56c68d8b87d4097da5bc0
Aditya Kumar <adityakumarsah4555@gmail.com>
2025-10-22 03:41:40 +0530

    fix some error in sort route.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ## command for start the server of python(uvicorn)-> uvicorn main:app --reload
 
-from fastapi import FastAPI,Path,HTTPException
+from fastapi import FastAPI,Path,HTTPException,Query
 ## HTTPException-> its is an special built-in exception ithingn Fast-api used for return custom HTTP error response when something is gone wrong
 import json
 
@@ -32,3 +32,16 @@ def view_patient(patient_id:str=Path(...,description='ID of patient in Database.
         return data[patient_id]
     # return {"Error":"Patient not found."} its always http status is ok 200
     raise HTTPException(status_code=404,detail="Patient not found")
+
+@app.get('/sort')
+def sort_data(sort_by:str=Query(...,description='Sort on the bases of height,weight and BMI'),order:str=Query('asc',description='sort in asc or desc order.')):
+    valid_rang=['Height','Weight','BMI']
+    if sort_by not in valid_rang:
+        raise HTTPException(status_code=400,detail='Invalid field selected from{valid_rang}')
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail='Invalid order select b/w asc and desc.')
+
+    data = load_data()
+    sort_order=True if order=='desc'else False
+    sorted_data=sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+    return sorted_data
